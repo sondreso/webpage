d93a13188a75ca60bf3f20218ed28c1d4a08d6f2
Sondre Sortland <sondreso.git@gmail.com>
2016-02-05 14:10:48 +0100

    Started on function for determining the currrent step

--- src/AppBundle/Controller/ControlPanelController.php
+++ src/AppBundle/Controller/ControlPanelController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\AppBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Exception;
@@ -20,6 +21,49 @@ class ControlPanelController extends Controller {
 			'departments' => $departments,
 		));
 			
+	}
+
+	public function determineCurrentStep($user){
+		$em = $this->getDoctrine()->getManager();
+
+		$department= $user->getFieldOfStudy()->getDepartment();
+		$semesters = $em->getRepository('AppBundle:Semester')->findAllSemestersByDepartment($department->getId());
+
+		$today = new DateTime("now");
+
+		$validSemesters = array();
+
+		foreach ($semesters as $semester) {
+
+			$semesterStartDate = $semester->getAdmissionStartDate();
+			$semesterEndDate = $semester->getAdmissionEndDate();
+
+			if ($semesterStartDate < $today && $today < $semesterEndDate) {
+				$validSemesters[] = $semester;
+			}
+		}
+
+		//Ingen gyldige semestre eksisterer, vi er på steg 0
+		if(empty($validSemesters)){
+			return 0;
+		}
+
+		//Kan ikke ha mer en et gyldig semester om gangen..,
+		if(count($validSemesters)>1){
+			return -1;
+		}
+
+		$semester = $validSemesters[0];
+
+		if($semester->getSemesterStartDate()<$today && $today > $semester->getAdmissionStartDate()){
+			return 1 + ($today->format('U')-$semester->getSemesterStartDate()->format('U'))/($semester->getAdmissionStartDate()->format('U') - $semester->getSemesterStartDate()>format('U'));
+		}
+
+		if($semester->getAdmissionEndDate()<$today && $today > $semester->getAdmissionStartDate()){
+			return 2 + ($today->format('U') - $semester->getAdmissionStartDate()->format('U'))/($semester->getAdmissionEndDate()->format('U') - $semester->getAdmissionStartDate()>format('U'));
+		}
+
+		return -1;
 	}
 
 }
